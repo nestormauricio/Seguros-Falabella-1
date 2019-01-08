f629aaf2518ec81a19423ffccfdf8b02ea32a582
Nestor Mauricio Perez Ruiz <nesperru@dmasterdorado.net>
2019-01-08 09:40:47 -0500

    cuarto

---
+++ WebApplicationAseguradorasB/WebApplicationAseguradorasB/Controllers/tbl_AseguradorasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationAseguradorasB.Models;
+
+namespace WebApplicationAseguradorasB.Controllers
+{
+    public class tbl_AseguradorasController : Controller
+    {
+        private bd_SegurosFalabellaEntities db = new bd_SegurosFalabellaEntities();
+
+        // GET: tbl_Aseguradoras
+        public ActionResult Index()
+        {
+            return View(db.tbl_Aseguradoras.ToList());
+        }
+
+        // GET: tbl_Aseguradoras/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_Aseguradoras tbl_Aseguradoras = db.tbl_Aseguradoras.Find(id);
+            if (tbl_Aseguradoras == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_Aseguradoras);
+        }
+
+        // GET: tbl_Aseguradoras/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: tbl_Aseguradoras/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "aseg_IdAseguradoraPk,aseg_NombreAseguradora")] tbl_Aseguradoras tbl_Aseguradoras)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbl_Aseguradoras.Add(tbl_Aseguradoras);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tbl_Aseguradoras);
+        }
+
+        // GET: tbl_Aseguradoras/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_Aseguradoras tbl_Aseguradoras = db.tbl_Aseguradoras.Find(id);
+            if (tbl_Aseguradoras == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_Aseguradoras);
+        }
+
+        // POST: tbl_Aseguradoras/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "aseg_IdAseguradoraPk,aseg_NombreAseguradora")] tbl_Aseguradoras tbl_Aseguradoras)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbl_Aseguradoras).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tbl_Aseguradoras);
+        }
+
+        // GET: tbl_Aseguradoras/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_Aseguradoras tbl_Aseguradoras = db.tbl_Aseguradoras.Find(id);
+            if (tbl_Aseguradoras == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_Aseguradoras);
+        }
+
+        // POST: tbl_Aseguradoras/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbl_Aseguradoras tbl_Aseguradoras = db.tbl_Aseguradoras.Find(id);
+            db.tbl_Aseguradoras.Remove(tbl_Aseguradoras);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplicationAseguradorasB/WebApplicationAseguradorasB/Controllers/tbl_ProductosController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationAseguradorasB.Models;
+
+namespace WebApplicationAseguradorasB.Controllers
+{
+    public class tbl_ProductosController : Controller
+    {
+        private bd_SegurosFalabellaEntities db = new bd_SegurosFalabellaEntities();
+
+        // GET: tbl_Productos
+        public ActionResult Index()
+        {
+            var tbl_Productos = db.tbl_Productos.Include(t => t.tbl_Aseguradoras);
+            return View(tbl_Productos.ToList());
+        }
+
+        // GET: tbl_Productos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_Productos tbl_Productos = db.tbl_Productos.Find(id);
+            if (tbl_Productos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_Productos);
+        }
+
+        // GET: tbl_Productos/Create
+        public ActionResult Create()
+        {
+            ViewBag.prod_IdAseguradora_Fk = new SelectList(db.tbl_Aseguradoras, "aseg_IdAseguradoraPk", "aseg_NombreAseguradora");
+            return View();
+        }
+
+        // POST: tbl_Productos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "prod_IdProductoPk,prod_Descripcion,prod_Prima,prod_Cobertura,prod_Asistencia,prod_MontoVenta,prod_IdAseguradora_Fk")] tbl_Productos tbl_Productos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbl_Productos.Add(tbl_Productos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.prod_IdAseguradora_Fk = new SelectList(db.tbl_Aseguradoras, "aseg_IdAseguradoraPk", "aseg_NombreAseguradora", tbl_Productos.prod_IdAseguradora_Fk);
+            return View(tbl_Productos);
+        }
+
+        // GET: tbl_Productos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_Productos tbl_Productos = db.tbl_Productos.Find(id);
+            if (tbl_Productos == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.prod_IdAseguradora_Fk = new SelectList(db.tbl_Aseguradoras, "aseg_IdAseguradoraPk", "aseg_NombreAseguradora", tbl_Productos.prod_IdAseguradora_Fk);
+            return View(tbl_Productos);
+        }
+
+        // POST: tbl_Productos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "prod_IdProductoPk,prod_Descripcion,prod_Prima,prod_Cobertura,prod_Asistencia,prod_MontoVenta,prod_IdAseguradora_Fk")] tbl_Productos tbl_Productos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbl_Productos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.prod_IdAseguradora_Fk = new SelectList(db.tbl_Aseguradoras, "aseg_IdAseguradoraPk", "aseg_NombreAseguradora", tbl_Productos.prod_IdAseguradora_Fk);
+            return View(tbl_Productos);
+        }
+
+        // GET: tbl_Productos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_Productos tbl_Productos = db.tbl_Productos.Find(id);
+            if (tbl_Productos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_Productos);
+        }
+
+        // POST: tbl_Productos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbl_Productos tbl_Productos = db.tbl_Productos.Find(id);
+            db.tbl_Productos.Remove(tbl_Productos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
